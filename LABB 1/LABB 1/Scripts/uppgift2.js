7aa55e9b8e55358938c8b83a4aea1c0031d81e4b
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 22:39:43 +0100

    Uppgift 2 av 5 klar.

---
+++ LABB 1/LABB 1/Scripts/uppgift2.js
@@ -0,0 +1,5 @@
+﻿function CalculateSalary() {
+    var baseSalary = 15000;
+    var provision = document.getElementById('saleSum').value * 0.09;
+    document.getElementById('totalSalary').innerHTML = baseSalary + provision;
+}
