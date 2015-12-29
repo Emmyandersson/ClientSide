65189253c84c505d679e4589fe77777b51f9553e
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 22:46:22 +0100

    Uppgift 3/5 klar.

---
+++ LABB 1/LABB 1/Scripts/uppgift3.js
@@ -0,0 +1,9 @@
+﻿function CalculateMinutesAndSeconds() {
+    var hours = document.getElementById('hours').value;
+    var minutes = hours * 60;
+    var seconds = minutes * 60;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+
+}
