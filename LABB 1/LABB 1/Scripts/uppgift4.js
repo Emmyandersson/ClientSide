67bc1aa572eb1c57c58480f0762f9b3205b356a8
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 23:03:30 +0100

    Uppgift 5/5 klar. Jag är inte helt 100 på uppgifterna då det känns som att det säkert finns lättare sätt att lösa dem på. Detta är något jag ska kolla upp.

--- LABB 1/LABB 1/Scripts/uppgift4.js
+++ LABB 1/LABB 1/Scripts/uppgift4.js
@@ -2,11 +2,15 @@
 //Denna koden känns väldigt stökig. 
 function CalculateSumAndAverage() {
     var numOne = document.getElementById('numOne').value;
+    //Här konverterar jag det som skickas in till en Number. Annars läses det som en string.
     var numberOne = Number(numOne);
+
     var numTwo = document.getElementById('numTwo').value;
     var numberTwo = Number(numTwo);
+
     var numThree = document.getElementById('numThree').value;
     var numberThree = Number(numThree);
+
     var sum = document.getElementById('sum').innerHTML = numberOne + numberTwo + numberThree;
     document.getElementById('average').innerHTML = sum / 3;
 }
---
+++ LABB 1/LABB 1/Scripts/uppgift5.js
@@ -0,0 +1,9 @@
+﻿
+function CalculateSwedishToDollarAndPound() {
+    var swedish = document.getElementById('swedish').value;
+    var dollar = swedish / 6;
+    var pound = swedish / 12;
+
+    document.getElementById('dollar').innerHTML = dollar;
+    document.getElementById('pound').innerHTML = pound;
+}
