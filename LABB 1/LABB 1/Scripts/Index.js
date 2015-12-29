8e92d30c533e0b697142084e3bbf7402a1213f9a
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 22:30:34 +0100

    Bytte till en textbox istället för hårdkodade värden.

--- LABB 1/LABB 1/Scripts/Index.js
+++ LABB 1/LABB 1/Scripts/Index.js
@@ -1,9 +1,7 @@
-﻿var tal = document.getElementById('tal').innerHTML;
-
-var squareResult = calculateSquare(tal);
-
-document.getElementById('answer').innerHTML = squareResult;
-
-function calculateSquare(tal) {
-    return Math.sqrt(tal);
+﻿//Funktionen fungerar såhär:
+// 1. jag sätter min variabel "tal" till värdet av vad som skickas in i min textbox (med id:et 'tal'. 
+//2. Därefter tilldelar jag värdet i min <p> tag med Id:et 'answer' till svaret - dvs Kvadratroten ur talet som skickades in. 
+function calculateSquare() {
+    var tal = document.getElementById('tal').value;
+    document.getElementById('answer').innerHTML = Math.sqrt(tal);
 }
