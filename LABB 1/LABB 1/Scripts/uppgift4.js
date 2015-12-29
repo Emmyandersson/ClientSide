a1eda9e772ef19893d679b43bc905944a4ace3d8
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 22:56:22 +0100

    Uppgift 4/5 klar. Den känns dock inte helt 100 även om den fungerar som förväntat.

---
+++ LABB 1/LABB 1/Scripts/uppgift4.js
@@ -0,0 +1,12 @@
+﻿
+//Denna koden känns väldigt stökig. 
+function CalculateSumAndAverage() {
+    var numOne = document.getElementById('numOne').value;
+    var numberOne = Number(numOne);
+    var numTwo = document.getElementById('numTwo').value;
+    var numberTwo = Number(numTwo);
+    var numThree = document.getElementById('numThree').value;
+    var numberThree = Number(numThree);
+    var sum = document.getElementById('sum').innerHTML = numberOne + numberTwo + numberThree;
+    document.getElementById('average').innerHTML = sum / 3;
+}
