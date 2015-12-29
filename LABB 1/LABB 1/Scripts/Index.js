16b0ff4bc49bc7ec6db00489cd8e290fd589f94d
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2015-12-29 21:58:54 +0100

    En uppgift på första labben klar.

---
+++ LABB 1/LABB 1/Scripts/Index.js
@@ -0,0 +1,8 @@
+﻿var tal = document.getElementById("tal").innerHTML;
+
+var squareResult = calculateSquare(tal);
+document.getElementById("answer").innerHTML = squareResult;
+
+function calculateSquare(tal) {
+    return tal^2;
+}
