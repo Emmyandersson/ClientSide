7ae1f3367f4d3d810e758e08f811df667f270ae8
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2016-01-07 14:48:06 +0100

    ordnat upp sökfunktionen. Sortering kvar. Håller även på med css

---
+++ LABB 2/Lab2_JS/WebApplication33/SearchIntegration/Scripts/index.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+    $('#searchInput').keyup(function () {
+        var searchField = $("#searchInput").val();
+        var meExp = new RegExp(searchField, "i");
+        $.getJSON('burger.json', function (data) {
+            var outPut = "<ul class='searchResults'> ";
+
+            $.each(data, function (key, value) {
+             
+                if ((value.name.search(meExp) !== -1) || (value.about.search(meExp) !== -1)) {
+                    outPut += "<li>" +
+                        "<p>" +
+                        "<strong>" + value.name + "</strong> " + value.about +
+                        "</p>" +
+                        "<img src='../Images/" + value.url_image + "' alt=' " + value.name + "' >" +
+                        "</li>";
+                }
+            });
+            outPut += "</ul>";
+            $('#update').html(outPut);
+
+        });
+    });
+});
